c722db3458ad3964159c203eaff3fafd39a579d0
Nabetani <takenori@nabetani.sakura.ne.jp>
2025-05-15 22:08:46 +0900

    use persist

--- tights-click/src/stage_store.ts
+++ tights-click/src/stage_store.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { StageIDType } from "./constants";
 
 export type StageUnit = {
@@ -16,16 +17,35 @@ type StageStore = {
   updateStageUnit: (id: StageIDType, newUnit: Partial<StageUnit>) => void;
 };
 
-export const useStageStore = create<StageStore>((set) => ({
-  stageStates: { m: {} },
-  updateStageUnit: (id, newUnit) =>
-    set((state) => ({
-      stageStates: {
-        ...state.stageStates,
-        m: {
-          ...state.stageStates.m,
-          [id]: { ...(state.stageStates.m[id] || { trialCount: 0, best: null, full: false }), ...newUnit },
+export const useStageStore = create<StageStore>()(
+  persist(
+    (set) => ({
+      stageStates: { m: {} },
+      updateStageUnit: (id, newUnit) =>
+        set((state) => ({
+          stageStates: {
+            ...state.stageStates,
+            m: {
+              ...state.stageStates.m,
+              [id]: { ...(state.stageStates.m[id] || { trialCount: 0, best: null, full: false }), ...newUnit },
+            },
+          },
+        })),
+    }),
+    {
+      name: "8nnjwst2t4", // unique name
+      storage: {
+        getItem: (name) => {
+          const value = localStorage.getItem(name);
+          return value ? JSON.parse(value) : null;
+        },
+        setItem: (name, value) => {
+          localStorage.setItem(name, JSON.stringify(value));
+        },
+        removeItem: (name) => {
+          localStorage.removeItem(name);
         },
       },
-    })),
-}));
+    }
+  )
+);
