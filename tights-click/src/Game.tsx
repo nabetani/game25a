9d795557b055c0eba095278532113a769c82549b
Nabetani <takenori@nabetani.sakura.ne.jp>
2025-05-17 23:22:51 +0900

    SVG で世界表現着手

--- tights-click/src/Game.tsx
+++ tights-click/src/Game.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
 import { StageIDType, splitStageID } from './constants';
 import { useCurrentGameStore } from './current_game_store';
+import useWorldStore from './worldStore';
 
 interface GameProps {
   stage: StageIDType | null;
+}
+
+
+function WorldSVG(): React.JSX.Element {
+  const { world } = useWorldStore()
+  const { width, height } = world
+  const pad = 0.5
+  const svgVW = width + pad * 2
+  const svgVH = height + pad * 2
+  const viewBox = [-pad, -pad, svgVW, svgVH].join(" ")
+  const vw = 90
+  const styleW = `${vw}vw`
+  const styleH = `${vw / svgVW * svgVH}vw`
+  return <svg className='world-svg' style={{ width: styleW, height: styleH }} viewBox={viewBox}>
+    {Array.from({ length: world.height }).map((_, y) =>
+      Array.from({ length: world.width }).map((_, x) => (
+        <g key={[x, y].join(" ")}>
+          <rect x={x} y={y} width={0.9} height={0.9} />
+        </g>
+      ))
+    )}
+  </svg>
 }
 
 const Game: React.FC<GameProps> = ({ stage }) => {
@@ -16,10 +39,13 @@ const Game: React.FC<GameProps> = ({ stage }) => {
 
   return (
     <div>
-      <p>Course: {course}</p>
-      <p>Size: {size}</p>
-      <p>Score: {currentGame.score}</p>
-      <p>Combo: {currentGame.combo}</p>
+      <div>
+        <p>Course: {course}</p>
+        <p>Size: {size}</p>
+        <p>Score: {currentGame.score}</p>
+        <p>Combo: {currentGame.combo}</p>
+      </div>
+      <WorldSVG />
     </div>
   );
 };
